1f31d5ba819c8294bdabcffc4817a42835b4f44b
MorNdour <ndmor@ept.sn>
2022-08-30 18:32:25 +0000

    une autre version

--- agritech/lib/accueil.dart
+++ agritech/lib/accueil.dart
@@ -19,11 +19,11 @@ class _AccueilState extends State<Accueil> {
     int _currentIndex = 0;
     final PageController controller = PageController();
     List images=[
-      {'maladie_atteinte':'healty','temperature':'30 C','humidite':'45 %','image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDuE2ejpy-CjPVNdAhuIVch-8DRr20pvVwxs2pBWtl&s'},
-      {'maladie_atteinte':'virus_zemoi','temperature':'37 C','humidite':'48%','image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfRQHBPjb91iTc0PNfJ25PfgjWkJo0uVdlRwoJpd2d&s'},
-      {'maladie_atteinte':'healty','temperature':'24 C','humidite':'35 %','image':'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'},
-      {'maladie_atteinte':'late_blight','temperature':'45 C','humidite':'60 %','image':'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},
-      {'maladie_atteinte':'healty','temperature':'28 C','humidite':'39 %','image':'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png'},
+      {'date':'2022-04-23','maladie_atteinte':'healty','temperature':'30 C','humidite':'45 %','image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDuE2ejpy-CjPVNdAhuIVch-8DRr20pvVwxs2pBWtl&s'},
+      {'date':'2022-04-24','maladie_atteinte':'virus_zemoi','temperature':'37 C','humidite':'48%','image':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfRQHBPjb91iTc0PNfJ25PfgjWkJo0uVdlRwoJpd2d&s'},
+      {'date':'2022-04-25','maladie_atteinte':'healty','temperature':'24 C','humidite':'35 %','image':'https://cdn.futura-sciences.com/buildsv6/images/wide1920/6/5/2/652a7adb1b_98148_01-intro-773.jpg'},
+      {'date':'2022-05-03','maladie_atteinte':'late_blight','temperature':'45 C','humidite':'60 %','image':'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'},
+      {'date':'2022-05-07','maladie_atteinte':'healty','temperature':'28 C','humidite':'39 %','image':'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png'},
     ];
     List<String> _date =['2022-04-23','2022-04-24','2022-04-25','2022-05-05','2022-05-07'];
     String _dateVal ;
@@ -44,31 +44,42 @@ class _AccueilState extends State<Accueil> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30.0,vertical: 30.0),
             child: Container(
-              padding: EdgeInsets.only(left: 10.0,right: 10.0),
-              decoration: BoxDecoration(border: Border.all(color: Colors.black,width: 2.0),
-              borderRadius: BorderRadius.circular(10.0)
-              ),
-              child: DropdownButton(
-                      hint: Text("select date",style: TextStyle(color: Colors.grey, fontSize: 22.0),),
-                      dropdownColor: Color.fromARGB(255, 151, 225, 153),
-                      elevation: 5,
-                      icon: Icon(Icons.arrow_drop_down),
-                      iconSize: 36,
-                      isExpanded: true,
-                      //value: _dateVal,
-                      style: TextStyle(color: Colors.black, fontSize: 22.0),
-                      onChanged: (value){
-                        setState(() {
-                          //_dateVal : value;
-                        });
-                      },
-                      items: _date.map((value){
-                        return DropdownMenuItem(
-                          value: value,
-                          child: Text(value,)
-                          );
-                      }).toList(),
+              padding: EdgeInsets.only(left: 0.0,right: 5.0),
+              child: Row (
+                  children: [
+                    Icon(Icons.calendar_month,
+                        size: 40, color: Colors.green,
                     ),
+                    Text(images[_currentIndex]['date'],style: TextStyle(color: Colors.grey, fontSize: 22.0),),
+                    Spacer(
+                        flex: 1
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 0.0),
+                      child: DropdownButton(
+                        hint: Text("select date",style: TextStyle(color: Colors.grey, fontSize: 22.0),),
+                        dropdownColor: Color.fromARGB(255, 151, 225, 153),
+                        elevation: 10,
+                        icon: Icon(Icons.arrow_drop_down),
+                        iconSize: 36,
+                        //isExpanded: true,
+                        //value: _dateVal,
+                        style: TextStyle(color: Colors.black, fontSize: 22.0),
+                        onChanged: (value){
+                          setState(() {
+                            //_dateVal : value;
+                          });
+                        },
+                        items: _date.map((value){
+                          return DropdownMenuItem(
+                              value: value,
+                              child: Text(value,)
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+              ),
             ),
           ),
           SizedBox(
@@ -100,49 +111,68 @@ class _AccueilState extends State<Accueil> {
             height: 20,
         ),
           Card(
-            elevation: 16,
+            elevation: 13,
             clipBehavior: Clip.antiAlias,
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             color: Colors.green,
             child: Column(
               children: [
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Row(
+                      Container(
+                        height:40,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(width: 1.0, color: Colors.grey),
+                          ),
+                        ),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children:[
+                                Text("Maladie atteinte",
+                                  style: TextStyle(color: Colors.white, fontSize: 22.0),
+                                ),
+                                Text(images[_currentIndex]['maladie_atteinte'],
+                                  style: TextStyle(color: Colors.white, fontSize: 22.0),
+                                ),
+                            ],
+                        ),
+                      ), 
+                      Container(
+                        height:40,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(width: 1.0, color: Colors.grey),
+                          ),
+                        ),
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children:[
-                              Text("$_currentIndex",
+                              Text("Tempearture",
                                 style: TextStyle(color: Colors.white, fontSize: 22.0),
                               ),
-                              Text(images[_currentIndex]['maladie_atteinte'],
+                              Text(images[_currentIndex]['temperature'],
                                 style: TextStyle(color: Colors.white, fontSize: 22.0),
                               ),
-                          ],  
-                      ), 
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                            Text("Tempearture",
-                              style: TextStyle(color: Colors.white, fontSize: 22.0),
-                            ),
-                            Text(images[_currentIndex]['temperature'],
-                              style: TextStyle(color: Colors.white, fontSize: 22.0),
-                            ),
-                        ],  
+                          ],
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children:[
-                            Text("humidite",
-                              style: TextStyle(color: Colors.white, fontSize: 22.0),
-                            ),
-                            Text(images[_currentIndex]['humidite'],
-                              style: TextStyle(color: Colors.white, fontSize: 22.0),
-                            ),
-                        ],  
+                      Container(
+                        height:40,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                              Text("humidite",
+                                style: TextStyle(color: Colors.white, fontSize: 22.0),
+                              ),
+                              Text(images[_currentIndex]['humidite'],
+                                style: TextStyle(color: Colors.white, fontSize: 22.0),
+                              ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
@@ -165,14 +195,15 @@ class _AccueilState extends State<Accueil> {
                       controller.jumpToPage(_currentIndex - 1);
                     },
                     icon: Icon(Icons.arrow_back),
-                    iconSize: 20.0,
+                    iconSize: 40.0,
                 ),
                 IconButton(
                   onPressed: (){
                     controller.jumpToPage(_currentIndex + 1);
                   },
                   icon: Icon(Icons.arrow_forward),
-                  iconSize: 20.0,
+                  iconSize: 40.0,
+                  color:Colors.black,
                 ),
               ],
             ),
